115e9fa589e204acfabe4284c270352762938c21
Kele Wang <nullcoke@gmail.com>
2023-10-04 17:20:19 +0800

    problem6 finished

--- hog/hog.py
+++ hog/hog.py
@@ -198,6 +198,11 @@ def always_roll(n):
     assert n >= 0 and n <= 10
     # BEGIN PROBLEM 6
     "*** YOUR CODE HERE ***"
+
+    def f(a, b):
+        return n
+
+    return f
     # END PROBLEM 6
 
 
